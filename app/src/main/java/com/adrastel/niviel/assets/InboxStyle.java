e5fd45a73d6f75c9223b05f8b1ae05adc851178e
HerelAdrastel <herel.dev@gmail.com>
2016-09-27 16:50:33 +0200

    CheckRecordService is working

---
+++ app/src/main/java/com/adrastel/niviel/assets/InboxStyle.java
@@ -0,0 +1,51 @@
+package com.adrastel.niviel.assets;
+
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+
+import com.adrastel.niviel.R;
+
+import java.util.ArrayList;
+
+public class InboxStyle extends NotificationCompat.InboxStyle {
+
+    private Context context;
+    private ArrayList<CharSequence> lines = new ArrayList<>();
+    private CharSequence title = null;
+    private CharSequence summary = null;
+
+    public InboxStyle(Context context) {
+        super();
+
+        this.context = context;
+    }
+
+    @Override
+    public NotificationCompat.InboxStyle setBigContentTitle(CharSequence title) {
+        this.title = title;
+        return super.setBigContentTitle(title);
+    }
+
+    @Override
+    public NotificationCompat.InboxStyle setSummaryText(CharSequence cs) {
+        this.summary = cs;
+        return super.setSummaryText(cs);
+    }
+
+    @Override
+    public NotificationCompat.InboxStyle addLine(CharSequence cs) {
+        lines.add(cs);
+        return super.addLine(cs);
+    }
+
+    @Override
+    public String toString() {
+        String inbox = String.format(context.getString(R.string.share_new_record_title), title, summary) + "\n";
+
+        for(CharSequence line : lines) {
+            inbox += (line + "\n");
+        }
+
+        return inbox;
+    }
+}
--- app/src/main/java/com/adrastel/niviel/services/CheckRecordService.java
+++ app/src/main/java/com/adrastel/niviel/services/CheckRecordService.java
@@ -4,16 +4,20 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.util.LongSparseArray;
 import android.support.v7.app.NotificationCompat;
 
 import com.adrastel.niviel.R;
 import com.adrastel.niviel.RecordModel;
 import com.adrastel.niviel.activities.MainActivity;
+import com.adrastel.niviel.activities.SettingsActivity;
 import com.adrastel.niviel.assets.Constants;
 import com.adrastel.niviel.assets.Cubes;
+import com.adrastel.niviel.assets.InboxStyle;
 import com.adrastel.niviel.assets.Log;
 import com.adrastel.niviel.database.DatabaseHelper;
 import com.adrastel.niviel.database.Follower;
@@ -43,6 +47,8 @@ public class CheckRecordService extends IntentService {
     public static final int COUNTRY = 2;
     public static final int CONTINENT = 3;
     public static final int WORLD = 4;
+
+    private static int notif_id = 0;
 
     /**
      * Stocke les messages des records pour les afficher dans la notification
@@ -102,26 +108,93 @@ public class CheckRecordService extends IntentService {
                 if(oldRecord.event().equals(newRecord.getEvent())) {
 
                     try {
-                        checkRank(newRecord, follower, SINGLE, COUNTRY, oldRecord.nr_single(), Long.parseLong(newRecord.getNr_single()));
+                        /*checkRank(newRecord, follower, SINGLE, COUNTRY, oldRecord.nr_single(), Long.parseLong(newRecord.getNr_single()));
                         checkRank(newRecord, follower, SINGLE, CONTINENT, oldRecord.cr_single(), Long.parseLong(newRecord.getCr_single()));
                         checkRank(newRecord, follower, SINGLE, WORLD, oldRecord.wr_single(), Long.parseLong(newRecord.getWr_single()));
                         checkRank(newRecord, follower, AVERAGE, COUNTRY, oldRecord.nr_average(), Long.parseLong(newRecord.getNr_average()));
                         checkRank(newRecord, follower, AVERAGE, CONTINENT, oldRecord.cr_average(), Long.parseLong(newRecord.getCr_average()));
-                        checkRank(newRecord, follower, AVERAGE, WORLD, oldRecord.wr_average(), Long.parseLong(newRecord.getWr_average()));
+                        checkRank(newRecord, follower, AVERAGE, WORLD, oldRecord.wr_average(), Long.parseLong(newRecord.getWr_average()));*/
 
                         //checkRecord(newRecord, follower, SINGLE, , Assets.dateToMillis(newRecord.getSingle()))
 
                         // Check for singles
-                        if(checkScore(Cubes.SINGLE, oldRecord, newRecord)) {
 
+                        // Savoir si il faut notifier
+                        boolean hasToNotify = false;
+
+                        // Corps de la notification
+                        String content = "";
+                        String nr = "";
+                        String cr = "";
+                        String wr = "";
+
+                        // Corps de la notification etendue
+                        InboxStyle inboxStyle = new InboxStyle(this);
+
+                        RecordModel.Marshal values = Record.FACTORY.marshal();
+
+                        if(singleChanged(oldRecord, newRecord)) {
+
+                            Log.v(oldRecord.single() + "->" + newRecord.getSingle());
+
+
+                            content = String.format(getString(R.string.notif_new_single), newRecord.getSingle());
+
+                            // Update notification title
+                            nr = newRecord.getNr_single();
+                            cr = newRecord.getCr_single();
+                            wr = newRecord.getWr_single();
+
+                            // Update database
+                            values.single(newRecord.getSingle());
+                            values.nr_single(Long.parseLong(newRecord.getNr_single()));
+                            values.cr_single(Long.parseLong(newRecord.getCr_single()));
+                            values.wr_single(Long.parseLong(newRecord.getWr_single()));
+
+                            hasToNotify = true;
 
                         }
 
                         // Check for average
-                        if(checkScore(Cubes.AVERAGE, oldRecord, newRecord)) {
+                        if(averageChanged(oldRecord, newRecord)) {
 
+                            Log.v(oldRecord.average() + "->" + newRecord.getAverage());
+                            // Si le titre est vide, on ne fait rien, sinon on ajoute un séparateur
+                            content = content.equals("") ? "" : content + " | ";
+                            content += String.format(getString(R.string.notif_new_average), newRecord.getAverage());
 
+                            // Update notification
+                            nr = nr.equals("") ? "" : nr + " | ";
+                            nr += newRecord.getNr_average();
 
+                            cr = cr.equals("") ? "" : cr + " | ";
+                            cr += newRecord.getCr_average();
+
+                            wr = wr.equals("") ? "" : wr + " | ";
+                            wr += newRecord.getWr_average();
+
+                            // Update database
+                            values.average(newRecord.getAverage());
+                            values.nr_average(Long.parseLong(newRecord.getNr_average()));
+                            values.cr_average(Long.parseLong(newRecord.getCr_average()));
+                            values.wr_average(Long.parseLong(newRecord.getWr_average()));
+
+                            hasToNotify = true;
+                        }
+
+                        if (hasToNotify) {
+
+                            inboxStyle.setBigContentTitle(follower.name());
+                            inboxStyle.setSummaryText(follower.wca_id());
+                            inboxStyle.addLine(content);
+                            inboxStyle.addLine(String.format(getString(R.string.record_nr), nr));
+                            inboxStyle.addLine(String.format(getString(R.string.record_cr), cr));
+                            inboxStyle.addLine(String.format(getString(R.string.record_wr), wr));
+
+                            makeNotification(follower, follower.name(), content, inboxStyle);
+
+                            // Update database
+                            //database.updateRecord(follower._id(), newRecord.getEvent(), values.asContentValues());
                         }
                     }
 
@@ -136,18 +209,58 @@ public class CheckRecordService extends IntentService {
 
     }
 
-    @SuppressWarnings("StringEquality")
-    private boolean checkScore(int type, Record oldRecord, com.adrastel.niviel.models.readable.Record newRecord) {
+    private void makeNotification(Follower follower, String title, String content, InboxStyle bigContent) {
 
-        if ((type == Cubes.SINGLE && oldRecord.single() != null && newRecord.getSingle() != null && oldRecord.single() != newRecord.getSingle())
-                || type == Cubes.AVERAGE && oldRecord.average() != null && newRecord.getAverage() != null && oldRecord.single() != newRecord.getAverage()) {
+        // Voir le profil
+        Intent gotoMainActivity = new Intent(this, MainActivity.class);
+        gotoMainActivity.putExtra(Constants.EXTRAS.FRAGMENT, ProfileFragment.FRAGMENT_TAG);
+        gotoMainActivity.putExtra(Constants.EXTRAS.WCA_ID, follower.wca_id());
+        gotoMainActivity.putExtra(Constants.EXTRAS.USERNAME, follower.name());
+
+        PendingIntent gotoMainActivityAction = PendingIntent.getActivity(this, 0, gotoMainActivity, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // Partager
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("text/plain");
+        share.putExtra(Intent.EXTRA_TEXT, bigContent.toString());
+
+        Intent shareChooser = Intent.createChooser(share, getString(R.string.share));
+        PendingIntent shareAction = PendingIntent.getActivity(this, 0, shareChooser, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // Parametres
+        Intent gotoSettings = new Intent(this, SettingsActivity.class);
+        PendingIntent gotoSettingsAction = PendingIntent.getActivity(this, 0, gotoSettings, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Builder notification = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setTicker(content)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS)
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setContentIntent(gotoMainActivityAction)
+                .addAction(R.drawable.ic_share, getString(R.string.share), shareAction)
+                .addAction(R.drawable.ic_settings, getString(R.string.settings), gotoSettingsAction)
+                .setStyle(bigContent);
 
 
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(notif_id, notification.build());
 
-            return true;
-        }
+        notif_id++;
+    }
 
-        return false;
+    @SuppressWarnings("ConstantConditions")
+    private boolean singleChanged(Record oldRecord, com.adrastel.niviel.models.readable.Record newRecord) {
+        return oldRecord.single() != null && newRecord.getSingle() != null && !oldRecord.single().equals(newRecord.getSingle());
+    }
+
+
+    @SuppressWarnings("ConstantConditions")
+    private boolean averageChanged(Record oldRecord, com.adrastel.niviel.models.readable.Record newRecord) {
+        return oldRecord.average() != null && newRecord.getAverage() != null && !oldRecord.average().equals(newRecord.getAverage());
     }
 
 
@@ -163,7 +276,7 @@ public class CheckRecordService extends IntentService {
         return false;
 
     }
-    //<editor-fold desc="notifyCheckRank">
+//<editor-fold desc="notifyCheckRank">
     private void notifyChangeRank(com.adrastel.niviel.models.readable.Record record, Follower follower, int type, int region, long oldRank, long newRank){
         // content values to update the record
         RecordModel.Marshal marshal = Record.FACTORY.marshal();
@@ -251,6 +364,7 @@ public class CheckRecordService extends IntentService {
     //</editor-fold>
 
 
+
     private void callData(String wca_id, final dataCallback callback) {
 
         OkHttpClient client = new OkHttpClient();
@@ -286,7 +400,8 @@ public class CheckRecordService extends IntentService {
 
                 try {
                     Log.d("nr", records.get(0).getNr_average());
-                    records.get(0).setNr_average("1");
+                    records.get(0).setSingle("3.60");
+                    records.get(0).setAverage("3.70");
                 }
                 catch (Exception e) {
                     e.printStackTrace();
